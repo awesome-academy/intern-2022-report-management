559690f90ca0025270a95743b41df9355e9808a6
duyvd <vu.duc.duy@sun-asterisk.com>
2022-06-17 08:00:14 +0700

    User Create Report

--- db/migrate/20220616020838_create_notifications.rb
+++ db/migrate/20220616020838_create_notifications.rb
@@ -2,7 +2,6 @@ class CreateNotifications < ActiveRecord::Migration[6.1]
   def change
     create_table :notifications do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :report, null: false, foreign_key: true
       t.boolean :unread
 
       t.timestamps
