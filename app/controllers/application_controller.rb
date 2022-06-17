8faaffd336860362e11e810007dd378ffdb87a37
duyvd <vu.duc.duy@sun-asterisk.com>
2022-06-17 08:00:14 +0700

    Login Logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+
   before_action :set_locale
 
   private
+
+  def redirect_root
+    return unless logged_in?
+
+    redirect_to root_path
+  end
+
+  def check_if_user_login
+    return if logged_in?
+
+    redirect_to login_path
+  end
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
 class HomeController < ApplicationController
+  before_action :check_if_user_login
+
   def index; end
 end
