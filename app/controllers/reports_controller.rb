94522eb1a4d37b78b75cc7c5f65e6454a3c431ec
duyvd <vu.duc.duy@sun-asterisk.com>
2022-06-17 08:00:14 +0700

    User show all report and filter report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,15 @@
 class ReportsController < ApplicationController
   before_action :check_if_user_login
+  before_action :find_report, except: %i(new create index)
 
-  def show; end
+  def index
+    @reports = current_user.reports.active.recent
+    if params[:filter]
+      @reports = @reports.by_status(params[:filter][:status])
+                         .by_created_at(params[:filter][:created_at])
+    end
+    @pagy, @reports = paginate_reports(@reports)
+  end
 
   def new
     @report = current_user.reports.build
@@ -23,5 +31,17 @@ class ReportsController < ApplicationController
 
   def report_params
     params.require(:report).permit Report::UPDATABLE_ATTRS
+  end
+
+  def paginate_reports reports
+    pagy(reports, items: Settings.report.items.item_per_page)
+  end
+
+  def find_report
+    @report = Report.find_by id: params[:id]
+    return if @report
+
+    flash[:danger] = t ".find_report_error"
+    redirect_to reports_path
   end
 end
