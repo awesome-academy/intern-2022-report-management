c93742f99cf10012e761b180199e847624f9e3c6
duyvd <vu.duc.duy@sun-asterisk.com>
2022-07-05 10:52:07 +0700

    Manager comment daily report

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Intern2022ReportManagement
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
     config.i18n.available_locales = [:en, :vi]
     config.i18n.default_locale = :en
+    config.middleware.use I18n::JS::Middleware
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,11 @@ Rails.application.routes.draw do
     put "/manager/reports", to: "manager/reports#update"
 
     namespace :manager do
-      resources :reports
+      resources :reports, only: %i(index show update)
     end
 
     resources :accounts, only: %i(index edit)
     resources :reports
+    resources :comments, only: :create
   end
 end
