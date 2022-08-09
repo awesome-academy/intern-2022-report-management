ef03a560c634042e28f706a0f135a9a615a74093
duyvd <vu.duc.duy@sun-asterisk.com>
2022-08-09 09:51:14 +0700

    Gem Ransack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,18 +21,6 @@ class ApplicationController < ActionController::Base
     redirect_to root_path
   end
 
-  def redirect_root
-    return unless logged_in?
-
-    redirect_to root_path
-  end
-
-  def require_login
-    return if logged_in?
-
-    redirect_to login_path
-  end
-
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
